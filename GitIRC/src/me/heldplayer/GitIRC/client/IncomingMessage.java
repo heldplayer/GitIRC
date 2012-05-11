714e95baad25a8a82382ad3b478cd290a4efaa01
heldplayer <mitchel.pyl@gmail.com>
2012-05-11 21:48:50 +0200

    Remove PING > PONG message

--- GitIRC/src/me/heldplayer/GitIRC/client/IncomingMessage.java
+++ GitIRC/src/me/heldplayer/GitIRC/client/IncomingMessage.java
@@ -43,7 +43,6 @@ public class IncomingMessage {
 	public boolean parse(MessageReciever reciever) {
 		if (type.equalsIgnoreCase("PING")) {
 			reciever.send("PONG " + argString);
-			System.out.println("PING > PONG");
 			return true;
 		}
 		if (type.equalsIgnoreCase("NOTICE")) {
