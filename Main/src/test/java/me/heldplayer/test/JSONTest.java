72d6b020ad6aef47240dfac21ee6e47bd5397caf
heldplayer <mitchel.pyl@gmail.com>
2014-04-19 21:00:19 +0200

    Add test for JSON parsing

---
+++ Main/src/test/java/me/heldplayer/test/JSONTest.java
@@ -0,0 +1,29 @@
+
+package me.heldplayer.test;
+
+import me.heldplayer.util.json.JSONException;
+import me.heldplayer.util.json.JSONObject;
+import me.heldplayer.util.json.JSONWriter;
+
+public class JSONTest {
+
+    public static void main(String[] args) {
+
+        try {
+            String input = "{true:false,\"null\":null,\"some\\\\ stuff\":[true, true, \"false\", 'true', 'null',{true:false,null:null}]}";
+            System.out.println(input);
+            JSONObject object = new JSONObject(input);
+
+            String resultString = JSONWriter.write(object);
+            System.out.println(resultString);
+            JSONObject newObject = new JSONObject(input);
+
+            String newResultString = JSONWriter.write(newObject);
+            System.out.println(newResultString);
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
