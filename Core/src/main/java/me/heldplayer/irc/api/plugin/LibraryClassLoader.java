48e58645a856764498c48820f1e0db0e9b6d64ab
heldplayer <mitchel.pyl@gmail.com>
2014-06-06 13:37:34 +0200

    Merge common ClassLoader code

--- Core/src/main/java/me/heldplayer/irc/api/plugin/LibraryClassLoader.java
+++ Core/src/main/java/me/heldplayer/irc/api/plugin/LibraryClassLoader.java
@@ -2,74 +2,12 @@
 package me.heldplayer.irc.api.plugin;
 
 import java.io.File;
-import java.io.IOException;
 import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.util.HashMap;
-import java.util.Set;
 
-import me.heldplayer.irc.api.IClassLoader;
-import sun.misc.IOUtils;
-
-public class LibraryClassLoader extends URLClassLoader implements IClassLoader {
-
-    private PluginLoader loader;
-    private String filename;
-
-    private HashMap<String, Class<?>> classes = new HashMap<String, Class<?>>();
+public class LibraryClassLoader extends CustomClassLoader {
 
     public LibraryClassLoader(PluginLoader loader, File file, ClassLoader parent) throws MalformedURLException {
-        super(file == null ? new URL[] {} : new URL[] { file.toURI().toURL() }, parent);
-
-        this.loader = loader;
-        this.filename = file.getName();
-    }
-
-    @Override
-    protected Class<?> findClass(String name) throws ClassNotFoundException {
-        return this.findClass(name, true);
-    }
-
-    Class<?> findClass(String name, boolean checkGlobal) throws ClassNotFoundException {
-        Class<?> result = classes.get(name);
-
-        if (result == null) {
-            if (checkGlobal) {
-                result = loader.findClass(name);
-            }
-
-            if (result == null) {
-                result = super.findClass(name);
-
-                if (result != null) {
-                    loader.setClass(name, result);
-                }
-            }
-
-            PluginLoader.log.info(String.format("[%s] Loaded class '%s'", this.filename, name));
-
-            classes.put(name, result);
-        }
-
-        return result;
-    }
-
-    Set<String> getClasses() {
-        return this.classes.keySet();
-    }
-
-    @Override
-    public byte[] findBytes(final String name) {
-        String str = name.replace('.', '/').concat(".class");
-        ClassLoader loader = this.getClass().getClassLoader();
-        try {
-            return IOUtils.readFully(loader.getResourceAsStream(str), -1, true);
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
+        super(loader, file, parent, file.getName());
     }
 
 }
--- Core/src/main/java/me/heldplayer/irc/api/plugin/PluginClassLoader.java
+++ Core/src/main/java/me/heldplayer/irc/api/plugin/PluginClassLoader.java
@@ -2,29 +2,17 @@
 package me.heldplayer.irc.api.plugin;
 
 import java.io.File;
-import java.io.IOException;
 import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.util.HashMap;
-import java.util.Set;
 import java.util.logging.Logger;
 
-import me.heldplayer.irc.api.IClassLoader;
-import sun.misc.IOUtils;
+public class PluginClassLoader extends CustomClassLoader {
 
-public class PluginClassLoader extends URLClassLoader implements IClassLoader {
-
-    private PluginLoader loader;
     private PluginInfo info;
     Plugin plugin;
 
-    private HashMap<String, Class<?>> classes = new HashMap<String, Class<?>>();
-
     public PluginClassLoader(PluginLoader loader, File file, PluginInfo info, ClassLoader parent) throws MalformedURLException {
-        super(file == null ? new URL[] {} : new URL[] { file.toURI().toURL() }, parent);
+        super(loader, file, parent, info.name);
 
-        this.loader = loader;
         this.info = info;
 
         try {
@@ -60,52 +48,6 @@ public class PluginClassLoader extends URLClassLoader implements IClassLoader {
 
     public PluginInfo getInfo() {
         return this.info;
-    }
-
-    @Override
-    protected Class<?> findClass(String name) throws ClassNotFoundException {
-        return this.findClass(name, true);
-    }
-
-    Class<?> findClass(String name, boolean checkGlobal) throws ClassNotFoundException {
-        Class<?> result = classes.get(name);
-
-        if (result == null) {
-            if (checkGlobal) {
-                result = loader.findClass(name);
-            }
-
-            if (result == null) {
-                result = super.findClass(name);
-
-                if (result != null) {
-                    loader.setClass(name, result);
-                }
-            }
-
-            PluginLoader.log.info(String.format("[%s] Loaded class '%s'", this.info.name, name));
-
-            classes.put(name, result);
-        }
-
-        return result;
-    }
-
-    Set<String> getClasses() {
-        return this.classes.keySet();
-    }
-
-    @Override
-    public byte[] findBytes(final String name) {
-        String str = name.replace('.', '/').concat(".class");
-        ClassLoader loader = this.getClass().getClassLoader();
-        try {
-            return IOUtils.readFully(loader.getResourceAsStream(str), -1, true);
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
     }
 
 }
